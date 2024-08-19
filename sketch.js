07ffdf6319f902a8f493b377d65b1b08dc85c0d5
Mah3221 <mariaheloisa31790@gmail.com>
2024-08-19 15:06:14 -0300

    Criando sketch.js

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//variáveis da bolinha//variáveis da bolinha
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 20;
+let raio = diametro / 2;
+
+let meusPontos = 0;
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//placar do jogo
+let pontosDoOponente = 0;
+
+
+//sons do jogo
+let raquetada;
+let ponto;
+let trilha;
